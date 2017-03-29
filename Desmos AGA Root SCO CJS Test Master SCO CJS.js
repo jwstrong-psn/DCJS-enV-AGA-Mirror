eb944e80eb8df2b2c0a197ad509bdcfacf035448
Strong, Joseph <joseph.strong@pearson.com>
2017-03-29 16:39:17 -0700

    A0596385

--- Desmos AGA Root SCO CJS Test Master SCO CJS.js
+++ Desmos AGA Root SCO CJS Test Master SCO CJS.js
@@ -140,6 +140,20 @@ PearsonGL.External.masterJS = (function() {
     return PearsonGL.External.rootJS.A0597598_A_labelAngle({'value':val,'name':name,'desmos':desmos,'log':console.log});
   }
 
+  //* functions for test purposes only
+  ts.A0596385_init = function(val, name, desmos) {
+    return PearsonGL.External.rootJS.A0596385_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  }
+
+  //* functions for test purposes only
+  ts.A0596385_updateAngles = function(val, name, desmos) {
+    return PearsonGL.External.rootJS.A0596385_updateAngles({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  }
+
+  //* functions for test purposes only
+  ts.A0596385_drawExtensions = function(val, name, desmos) {
+    return PearsonGL.External.rootJS.A0596385_drawExtensions({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  }
   
   Object.assign(exports,flattenFuncStruct(ts));
 
