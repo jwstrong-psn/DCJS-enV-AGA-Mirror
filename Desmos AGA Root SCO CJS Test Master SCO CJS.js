6dfd7510b7421f881a18b535691951c77d121d4e
Strong, Joseph <joseph.strong@pearson.com>
2017-04-11 17:48:17 -0700

    A0597616

--- Desmos AGA Root SCO CJS Test Master SCO CJS.js
+++ Desmos AGA Root SCO CJS Test Master SCO CJS.js
@@ -171,6 +171,11 @@ PearsonGL.External.masterJS = (function() {
   }
 
   //* functions for test purposes only
+  ts.A0597616_label = function(val, name, desmos) {
+    return PearsonGL.External.rootJS.A0597616_label({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  }
+
+  //* functions for test purposes only
   ts.A0597720_labelEquation = function(val, name, desmos) {
     return PearsonGL.External.rootJS.A0597720_labelEquation({'value':val,'name':name,'desmos':desmos,'log':console.log});
   }
