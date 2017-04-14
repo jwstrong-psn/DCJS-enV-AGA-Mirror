07baaee2307a4d101c32e667b37205761b5da7cd
Chris Anton <christopher.anton@pearson.com>
2017-04-14 12:34:01 -0700

    A0597544 instead of 545

--- Desmos AGA Root SCO CJS Test Master SCO CJS.js
+++ Desmos AGA Root SCO CJS Test Master SCO CJS.js
@@ -151,13 +151,13 @@ PearsonGL.External.masterJS = (function() {
   }
 
   //* functions for test purposes only
-  ts.A0597545_init = function(val, name, desmos) {
-    return PearsonGL.External.rootJS.A0597545_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597544_init = function(val, name, desmos) {
+    return PearsonGL.External.rootJS.A0597544_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
   }
 
   //* functions for test purposes only
-  ts.A0597545_updateLabels = function(val, name, desmos) {
-    return PearsonGL.External.rootJS.A0597545_updateLabels({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597544_updateLabels = function(val, name, desmos) {
+    return PearsonGL.External.rootJS.A0597544_updateLabels({'value':val,'name':name,'desmos':desmos,'log':console.log});
   }
 
   //* functions for test purposes only
