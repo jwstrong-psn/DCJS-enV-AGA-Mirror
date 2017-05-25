e2bae7804c5add0ab5c19ea0f87fe391fcaaa8dc
Strong, Joseph <joseph.strong@pearson.com>
2017-05-25 15:14:35 -0700

    Fixed the part where I used arrow notation in exactly the wrong way.

--- Desmos AGA Root SCO CJS Test Master SCO CJS.js
+++ Desmos AGA Root SCO CJS Test Master SCO CJS.js
@@ -57,8 +57,8 @@ PearsonGL.External.masterJS = (function() {
         observeZoom:''}, // Tracks zoom level in units/px as x_{pxScale} and y_{pxScale}
       label:{} // Label functions are used for labeling points with certain values
     },
-    sliderVal:function(val,name,desmos){return PearsonGL.External.rootJS.shared_label_valueOnly({'value':val,'name':name,'desmos':desmos,'log':console.log});}
-  }
+    sliderVal:function(val,name,desmos){return PearsonGL.External.rootJS.shared_label_valueOnly((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);}
+  };
 
   /***********************************************************************************
    * EXPORTS / PUBLIC FUNCTIONS
@@ -86,11 +86,11 @@ PearsonGL.External.masterJS = (function() {
   **********************************************************************************/
 
   exports.reflectParabola = function(val, name, desmos) {
-    console.log('redirecting from Master to Root SCO for testing')
-    return PearsonGL.External.rootJS.reflectParabola({'value':val,'name':name,'desmos':desmos,'log':console.log});
+    console.log('redirecting from Master to Root SCO for testing');
+    return PearsonGL.External.rootJS.reflectParabola((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
     //console.log(name + " was updated to " + val);
     //desmos.setExpression({id: 'reflected', latex: 'y=-a(x-' + val + ')^2-k', color: '#00AA00'});
-  }
+  };
 
   /** FUNCTIONS TRANSLATE OLD API (value, name, desmos)
       INTO NEW API ({value:…,name:…,desmos:…,uniqueId:…})
@@ -98,111 +98,111 @@ PearsonGL.External.masterJS = (function() {
       DELETE ALL WHEN API IS UPDATED
       **/
 
-  ts.shared.init.observeZoom = (val, name, desmos)=>PearsonGL.External.rootJS.shared_init_observeZoom({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.shared.init.observeZoom = (val, name, desmos)=>PearsonGL.External.rootJS.shared_init_observeZoom((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.shared.init.shareState = (val, name, desmos)=>PearsonGL.External.rootJS.shared_init_shareState({'value':val,'name':name,'desmos':desmos,'log':console.log, 'uniqueId':'shared'});
+  ts.shared.init.shareState = (val, name, desmos)=>PearsonGL.External.rootJS.shared_init_shareState((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log, 'uniqueId':'shared'}:val);
 
-  ts.shared.label.labelEquation = (val, name, desmos)=>PearsonGL.External.rootJS.shared_label_labelEquation({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.shared.label.labelEquation = (val, name, desmos)=>PearsonGL.External.rootJS.shared_label_labelEquation((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.shared.label.valueOnly = (val, name, desmos)=>PearsonGL.External.rootJS.shared_label_valueOnly({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.shared.label.valueOnly = (val, name, desmos)=>PearsonGL.External.rootJS.shared_label_valueOnly((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.shared.label.labelAngle = (val, name, desmos)=>PearsonGL.External.rootJS.shared_label_labelAngle({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.shared.label.labelAngle = (val, name, desmos)=>PearsonGL.External.rootJS.shared_label_labelAngle((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.shared.label.labelTriAngles = (val, name, desmos)=>PearsonGL.External.rootJS.shared_label_labelTriAngles({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.shared.label.labelTriAngles = (val, name, desmos)=>PearsonGL.External.rootJS.shared_label_labelTriAngles((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597514_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597514_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597514_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597514_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597514_updateLabels = (val, name, desmos)=>PearsonGL.External.rootJS.A0597514_updateLabels({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597514_updateLabels = (val, name, desmos)=>PearsonGL.External.rootJS.A0597514_updateLabels((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597534_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597534_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597534_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597534_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597534_updateLabels = (val, name, desmos)=>PearsonGL.External.rootJS.A0597534_updateLabels({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597534_updateLabels = (val, name, desmos)=>PearsonGL.External.rootJS.A0597534_updateLabels((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597552_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597552_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597552_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597552_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597552_updateLabels = (val, name, desmos)=>PearsonGL.External.rootJS.A0597552_updateLabels({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597552_updateLabels = (val, name, desmos)=>PearsonGL.External.rootJS.A0597552_updateLabels((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597534_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597534_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597534_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597534_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597534_updateLabels = (val, name, desmos)=>PearsonGL.External.rootJS.A0597534_updateLabels({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597534_updateLabels = (val, name, desmos)=>PearsonGL.External.rootJS.A0597534_updateLabels((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597544_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597544_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597544_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597544_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597544_updateLabels = (val, name, desmos)=>PearsonGL.External.rootJS.A0597544_updateLabels({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597544_updateLabels = (val, name, desmos)=>PearsonGL.External.rootJS.A0597544_updateLabels((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597546_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597546_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597546_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597546_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597546_updateLabels = (val, name, desmos)=>PearsonGL.External.rootJS.A0597546_updateLabels({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597546_updateLabels = (val, name, desmos)=>PearsonGL.External.rootJS.A0597546_updateLabels((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597772_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597772_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597772_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597772_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597789_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597789_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597789_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597789_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597789_volumeCone = (val, name, desmos)=>PearsonGL.External.rootJS.A0597789_volumeCone({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597789_volumeCone = (val, name, desmos)=>PearsonGL.External.rootJS.A0597789_volumeCone((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597789_volumeStack = (val, name, desmos)=>PearsonGL.External.rootJS.A0597789_volumeStack({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597789_volumeStack = (val, name, desmos)=>PearsonGL.External.rootJS.A0597789_volumeStack((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597768_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597768_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597768_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597768_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597777_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597777_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597777_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597777_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0598652_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0598652_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0598652_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0598652_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0598652_updateLabels = (val, name, desmos)=>PearsonGL.External.rootJS.A0598652_updateLabels({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0598652_updateLabels = (val, name, desmos)=>PearsonGL.External.rootJS.A0598652_updateLabels((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597714_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597714_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597714_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597714_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597773_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597773_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597773_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597773_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0596373_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0596373_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0596373_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0596373_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0596373_changeStep = (val, name, desmos)=>PearsonGL.External.rootJS.A0596373_changeStep((arguments.length>1)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
+  ts.A0596373_changeStep = (val, name, desmos)=>PearsonGL.External.rootJS.A0596373_changeStep((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0596370_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0596370_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0596370_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0596370_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0596370_changeStep = (val, name, desmos)=>PearsonGL.External.rootJS.A0596370_changeStep((arguments.length>1)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
+  ts.A0596370_changeStep = (val, name, desmos)=>PearsonGL.External.rootJS.A0596370_changeStep((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597714_updateLabels = (val, name, desmos)=>PearsonGL.External.rootJS.A0597714_updateLabels({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597714_updateLabels = (val, name, desmos)=>PearsonGL.External.rootJS.A0597714_updateLabels((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597538_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597538_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597538_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597538_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597538_updateLabels = (val, name, desmos)=>PearsonGL.External.rootJS.A0597538_updateLabels({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597538_updateLabels = (val, name, desmos)=>PearsonGL.External.rootJS.A0597538_updateLabels((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0598832_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0598832_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0598832_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0598832_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0598832_changeLineType = (val, name, desmos)=>PearsonGL.External.rootJS.A0598832_changeLineType({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0598832_changeLineType = (val, name, desmos)=>PearsonGL.External.rootJS.A0598832_changeLineType((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0598832_regionsAddRemove = (val, name, desmos)=>PearsonGL.External.rootJS.A0598832_regionsAddRemove({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0598832_regionsAddRemove = (val, name, desmos)=>PearsonGL.External.rootJS.A0598832_regionsAddRemove((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597616_label = (val, name, desmos)=>PearsonGL.External.rootJS.A0597616_label({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597616_label = (val, name, desmos)=>PearsonGL.External.rootJS.A0597616_label((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597616_label_noCorrection = (val, name, desmos)=>PearsonGL.External.rootJS.A0597616_label_noCorrection({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597616_label_noCorrection = (val, name, desmos)=>PearsonGL.External.rootJS.A0597616_label_noCorrection((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597720_labelEquation = (val, name, desmos)=>PearsonGL.External.rootJS.A0597720_labelEquation({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597720_labelEquation = (val, name, desmos)=>PearsonGL.External.rootJS.A0597720_labelEquation((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597598_A_labelAngle = (val, name, desmos)=>PearsonGL.External.rootJS.A0597598_A_labelAngle({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597598_A_labelAngle = (val, name, desmos)=>PearsonGL.External.rootJS.A0597598_A_labelAngle((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0596385_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0596385_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0596385_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0596385_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597503_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597503_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597503_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597503_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0596385_updateAngles = (val, name, desmos)=>PearsonGL.External.rootJS.A0596385_updateAngles({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0596385_updateAngles = (val, name, desmos)=>PearsonGL.External.rootJS.A0596385_updateAngles((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0596392_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0596392_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0596392_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0596392_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597629_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597629_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597629_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597629_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597630_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597630_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597630_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597630_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597631_equation = (val, name, desmos)=>PearsonGL.External.rootJS.A0597631_equation({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597631_equation = (val, name, desmos)=>PearsonGL.External.rootJS.A0597631_equation((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597634_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597634_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597634_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597634_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0596385_drawExtensions = (val, name, desmos)=>PearsonGL.External.rootJS.A0596385_drawExtensions({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0596385_drawExtensions = (val, name, desmos)=>PearsonGL.External.rootJS.A0596385_drawExtensions((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597724_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597724_init({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597724_init = (val, name, desmos)=>PearsonGL.External.rootJS.A0597724_init((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
-  ts.A0597724_dragging = (val, name, desmos)=>PearsonGL.External.rootJS.A0597724_dragging({'value':val,'name':name,'desmos':desmos,'log':console.log});
+  ts.A0597724_dragging = (val, name, desmos)=>PearsonGL.External.rootJS.A0597724_dragging((desmos!==undefined)?{'value':val,'name':name,'desmos':desmos,'log':console.log}:val);
 
   exports.gcf = function(arr) {
     var arr = arr.value;
